2172a6d33b07b862fca0c5c5435c943088faf3c1
DELatina <115489056+DELatina@users.noreply.github.com>
2022-11-12 18:47:30 +0300

    Лабораторная работа 5

---
+++ Лабораторная работа 5/task3.py
@@ -0,0 +1,9 @@
+def get_unique_list_numbers() -> list[int]:
+    from random import randint
+    list_unique_numbers = set([randint(-10, 10) for i in range(16)])
+    return list_unique_numbers
+
+
+list_unique_numbers = get_unique_list_numbers()
+print(list_unique_numbers)
+print(len(list_unique_numbers) == len(set(list_unique_numbers)))
